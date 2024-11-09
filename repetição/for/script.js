55df9b0b9a8fbc75a1744b294d600e3feddd95de
leuxtc <l3oxon3@gmail.com>
2024-11-09 12:04:39 -0300

    Estrutura de repetição

---
+++ repetição/for/script.js
@@ -0,0 +1,8 @@
+let pontosVida = 0
+
+for (i = 0; i <= 9; i++){
+    pontosVida += 1
+    console.log("Você tomou uma poção e está com " + pontosVida + " de vida")
+}
+
+console.log("Total de vida: " + pontosVida)
